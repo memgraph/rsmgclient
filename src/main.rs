1a21c92d0eec64bc4c8f55cbdbe1a1c2e4ecdbd2
Mihael Jaic <mihael.jaic@memgraph.io>
2020-07-20 09:33:25 +0200

    Add example of using Connection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use rsmgclient::{MgValue, connect};
+
 fn main() {
-    let x=1;
-    let y = 2;
-    if x <= y {}
-    println!("Hello, world!");
+    let connection = match connect("127.0.0.1", 7687) {
+        Ok(c) => c,
+        Err(err) => panic!("{}", err),
+    };
+
+    let rows: Vec<Vec<MgValue>> = match connection.execute("CREATE (n:Person {name: 'John'})-[e:KNOWS]->(m:Person {name: 'Steve'}) RETURN n, e, m;") {
+        Ok(res) => res,
+        Err(err) => panic!("Query failed: {}", err),
+    };
+
+    for (index, row) in rows.iter().enumerate() {
+        for val in row {
+            println!("{}", val);
+        }
+    }
 }
