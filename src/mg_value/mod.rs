755a56776664aca450c6ac4264385621e4bb3b17
Mihael Jaic <mihael.jaic@memgraph.io>
2020-07-23 11:05:14 +0200

    Add missing licenses

--- src/mg_value/mod.rs
+++ src/mg_value/mod.rs
@@ -1,3 +1,17 @@
+// Copyright (c) 2016-2020 Memgraph Ltd. [https://memgraph.com]
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use super::bindings;
 use std::collections::HashMap;
 use std::ffi::CStr;
@@ -490,7 +504,7 @@ impl fmt::Display for MgUnboundRelationship {
 }
 
 impl fmt::Display for MgPath {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, _f: &mut Formatter<'_>) -> fmt::Result {
         unimplemented!();
     }
 }
