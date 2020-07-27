b0393167bfb4f6de448ec02586da9c00bf879597
Mihael Jaic <mihael.jaic@memgraph.io>
2020-07-27 21:04:26 +0200

    Add example of using query params

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use rsmgclient::{connect, MgValue};
+use rsmgclient::{connect, MgValue, QueryParam};
+use std::collections::HashMap;
 
 fn main() {
     let connection = match connect("127.0.0.1", 7687) {
         Ok(c) => c,
         Err(err) => panic!("{}", err),
     };
+    let mut map: HashMap<String, QueryParam> = HashMap::new();
+    map.insert(String::from("address"), QueryParam::Null);
+    map.insert(String::from("is_programmer"), QueryParam::Bool(true));
+    map.insert(
+        String::from("name"),
+        QueryParam::String(String::from("James Bond")),
+    );
+    map.insert(
+        String::from("list"),
+        QueryParam::List(vec![QueryParam::String(String::from("val"))]),
+    );
+
+    let mut params: HashMap<String, QueryParam> = HashMap::new();
+    params.insert(String::from("real_params"), QueryParam::Map(map));
 
     let rows: Vec<Vec<MgValue>> = match connection.execute(
         "CREATE (n:Person {name: 'John'})-[e:KNOWS]->(m:Person {name: 'Steve'}) RETURN n, e, m;",
+        Some(&params),
     ) {
         Ok(res) => res,
         Err(err) => panic!("Query failed: {}", err),
