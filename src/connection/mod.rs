7015101a80b1add3237760228c93ef2856ded6b4
dejankos <kosdejan@yahoo.com>
2021-04-26 17:39:57 +0200

    removed unnecessary panic

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -500,16 +500,21 @@ impl Connection {
                     Err(err)
                 }
             },
-            false => match &mut self.results_iter {
-                Some(it) => match it.next() {
-                    Some(x) => Ok(Some(x)),
-                    None => {
-                        self.status = ConnectionStatus::Ready;
-                        Ok(None)
-                    }
-                },
-                None => panic!(),
-            },
+            false => match self.next_record() {
+                Some(x) => Ok(Some(x)),
+                None => {
+                    self.status = ConnectionStatus::Ready;
+                    Ok(None)
+                }
+            }
+        }
+    }
+
+    fn next_record(&mut self) -> Option<Record> {
+        if let Some(iter) = self.results_iter.as_mut() {
+            iter.next()
+        } else {
+            None
         }
     }
 
